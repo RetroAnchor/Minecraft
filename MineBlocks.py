c5c1c3be9ba9535010cc59980937730fed6ebf3f
Retro <144626522+RetroAnchor@users.noreply.github.com>
2023-11-29 11:33:51 -0500

    MineBlocks.py
    Mines blocks within a 100 pixel radius now. (STILL JUST CLICKS AND HOLDS) (NEW KEYBIND IS P)

--- MineBlocks.py
+++ MineBlocks.py
@@ -2,16 +2,16 @@ import pyautogui
 import time
 import keyboard
 
-# Function to click and hold, moving in a 500-pixel square
+# Function to click and hold, moving in a 100-pixel square
 def click_and_hold_square(start_x, start_y):
     try:
-        print("Press P to start clicking and holding in a 500-pixel square.")
+        print("Press P to start clicking and holding in a 100-pixel square.")
         
         # Wait for the user to press P
         keyboard.wait('p')
 
         # Set the size of the square
-        square_size = 500
+        square_size = 100
 
         # Move the mouse to the starting position
         pyautogui.moveTo(start_x, start_y, duration=1)
@@ -19,7 +19,7 @@ def click_and_hold_square(start_x, start_y):
         # Click and hold
         pyautogui.mouseDown()
 
-        print("Clicking and holding in a 500-pixel square. Press Ctrl+C to stop.")
+        print("Clicking and holding in a 100-pixel square. Press Ctrl+C to stop.")
 
         # Move in a square pattern
         for _ in range(4):
@@ -34,17 +34,17 @@ def click_and_hold_square(start_x, start_y):
 
         # Release the mouse button
         pyautogui.mouseUp()
-        print("\nClicking and holding in a 500-pixel square stopped.")
+        print("\nClicking and holding in a 100-pixel square stopped.")
 
     except KeyboardInterrupt:
         # Release the mouse button if the script is interrupted
         pyautogui.mouseUp()
-        print("\nClicking and holding in a 500-pixel square stopped.")
+        print("\nClicking and holding in a 100-pixel square stopped.")
 
 # Define the starting position (adjust these coordinates)
-start_x = 500
-start_y = 500
+start_x = 100
+start_y = 100
 
-# Call the function to click and hold, moving in a 500-pixel square, when P is pressed
+# Call the function to click and hold, moving in a 100-pixel square, when P is pressed
 click_and_hold_square(start_x, start_y)
 
